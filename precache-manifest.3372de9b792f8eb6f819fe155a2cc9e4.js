388844898627ad3e838d71137d067196e2627acc
Irene Perez <inreallifeirene@gmail.com>
2020-09-03 16:52:52 -0700

    Updates

---
+++ precache-manifest.3372de9b792f8eb6f819fe155a2cc9e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53792baccc2fc19910b100790b8831a2",
+    "url": "/poke-library/index.html"
+  },
+  {
+    "revision": "112505495b5770b2c4c4",
+    "url": "/poke-library/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b7c791c66473754fe0e9",
+    "url": "/poke-library/static/css/main.13f0edbc.chunk.css"
+  },
+  {
+    "revision": "112505495b5770b2c4c4",
+    "url": "/poke-library/static/js/2.3b47c0a0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/poke-library/static/js/2.3b47c0a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7c791c66473754fe0e9",
+    "url": "/poke-library/static/js/main.70e10aab.chunk.js"
+  },
+  {
+    "revision": "ed03ebc646a334a3cbdb",
+    "url": "/poke-library/static/js/runtime-main.8d86a19c.js"
+  },
+  {
+    "revision": "76bf7c6c735e6f39499557b6e7f13e0e",
+    "url": "/poke-library/static/media/pokeball.76bf7c6c.png"
+  }
+]);
